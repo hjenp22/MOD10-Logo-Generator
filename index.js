bcd3aef017e4f73fbf74227fae492ea484ef4082
hjenp22 <hjipark22@gmail.com>
2023-12-12 22:31:55 -0600

    messy fix

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 
-class LogoGeerator {
-    async generateLogo() {
-        const text = await this.textPrompter.promtText();
-        const textColor = await this.colorPicker.pickColor();
-        const shape = await this.shape.picker.picksShape();
-        const shapeColor = await this.colorPicker.pickColor();
+// class LogoGeerator {
+//     async generateLogo() {
+//         const text = await this.textPrompter.promtText();
+//         const textColor = await this.colorPicker.pickColor();
+//         const shape = await this.shape.picker.picksShape();
+//         const shapeColor = await this.colorPicker.pickColor();
 
-        const svgCode = this.svgRenderer.renderSVG(text, textColor, shape, shapeColor);
-        this.fileSaver.saveToFile(svgCode, 'logo.svg');
+//         const svgCode = this.svgRenderer.renderSVG(text, textColor, shape, shapeColor);
+//         this.fileSaver.saveToFile(svgCode, 'logo.svg');
 
-        console.log('Generated logo.svg');
-    }
-}
+//         console.log('Generated logo.svg');
+//     }
+// }
 
 class ColorPicker {
     async pickColor() {
@@ -30,7 +30,7 @@ class ColorPicker {
 
 class ShapePicker {
     async pickShape() {
-        const amswers = await inquirer.prompt([
+        const answers = await inquirer.prompt([
             {
                 type: 'list',
                 name: 'shape',
@@ -55,9 +55,23 @@ class TextPrompter {
     }
 }
 
-class SVGRenderer {
+function SVGRenderer {
     renderSVG(text, textColor, shape, shapeColor){
-        //implementations for rendering svg code
+        let newShape;
+        switch (shape) {
+            case "Triangle":
+                newShape= new Triangle(text, textColor, shape, shapeColor);
+                break;
+            case "Circle":
+                newShape= new Circle(text, textColor, shape, shapeColor);
+                break;
+            case "Square":;
+                newShape= new Square(text, textColor, shape, shapeColor);
+                break;
+            Default:
+                console.log('not a valid shape');
+    
+        }
     }
 }
 class FileSaver{
