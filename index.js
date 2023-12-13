29cec1eaff8e71b5ba3581aef4f17beac3149e9d
hjenp22 <hjipark22@gmail.com>
2023-12-12 23:18:47 -0600

    need to finish hw

--- index.js
+++ index.js
@@ -1,66 +1,69 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
-
 const { Triangle, Circle, Square } = require('./lib/Shape');
 
-
 class LogoGenerator {
-     async generateLogo() {
-        const answers = await inquirer.prompt([
-            {
-                type: 'input',
-                name: 'text',
-                message: 'Enter text (up to three characters):',
-                validate: (input) => input.length <= 3,
-            },
-            {
-                type: 'input',
-                name: 'textColor',
-                message: 'Enter text color (keyword or hexadecimal)',
-            },
-            {
-                type: 'list',
-                name: 'shape',
-                message: 'Choose a shape:',
-                choices: ['circle', 'triangle', 'square'],
-            },
-            {
-                type:'input',
-                name: 'shapeColor',
-                message: 'Enter shape color (keyword or hexadecimal)',
-            }
-        ]);
-        this.generateSVG(answers.shape, answers.text, answers.textcolor, answers.shapecolor);
-     }
+  async generateLogo() {
+    const answers = await inquirer.prompt([
+      {
+        type: 'input',
+        name: 'text',
+        message: 'Enter text (up to three characters):',
+        validate: (input) => input.length <= 3,
+      },
+      {
+        type: 'input',
+        name: 'textColor',
+        message: 'Enter text color (keyword or hexadecimal)',
+      },
+      {
+        type: 'list',
+        name: 'shape',
+        message: 'Choose a shape:',
+        choices: ['circle', 'triangle', 'square'],
+      },
+      {
+        type: 'input',
+        name: 'shapeColor',
+        message: 'Enter shape color (keyword or hexadecimal)',
+      },
+    ]);
 
+    this.generateSVG(answers.shape, answers.text, answers.textColor, answers.shapeColor);
+  }
 
-    generateSVG(text, textColor, shape, shapeColor){
-        let newShape;
-        switch (shape) {
-            case "Triangle":
-                newShape= new Triangle(text, textColor, shape, shapeColor);
-                break;
-            case "Circle":
-                newShape= new Circle(text, textColor, shape, shapeColor);
-                break;
-            case "Square":;
-                newShape= new Square(text, textColor, shape, shapeColor);
-                break;
-            default:
-                console.log('not a valid shape');
-                return;
-        }
-    };
+  generateSVG(shape, text, textColor, shapeColor) {
+    let newShape;
+    switch (shape) {
+      case 'triangle':
+        newShape = new Triangle(text, textColor, shapeColor);
+        break;
+      case 'circle':
+        newShape = new Circle(text, textColor, shapeColor);
+        break;
+      case 'square':
+        newShape = new Square(text, textColor, shapeColor);
+        break;
+      default:
+        console.log('Not a valid shape');
+        return;
+    }
+
+    fs.writeFile('logo.svg', newShape.render(), (err) => {
+      if (err) {
+        console.error('Error creating logo.svg:', err);
+      } else {
+        console.log('Generated logo.svg');
+      }
+    });
+  }
 }
-    
-    fs.writeFile('logo.svg', newShape.render(), (err)=> {
-        if (err) {
-            console.error('Error creating logo.svg:', err);
-        } else {
-            console.log('generated logo.svg');
-        }
-    })
 
-
-const LogoGenerator = new LogoGenerator();
-LogoGenerator.generateLogo();
+inquirer.prompt(questions).then((response)=> {
+    generateSVG(
+        response.shape,
+        response.text,
+        response.textColor,
+        response.shapeColor
+    );
+});
